caaab51029a7507b04bd41f60fa7429afbcc5631
Andrew Back <andrew@abopen.com>
2021-10-31 20:01:33 +0000

    Start DS branding.

--- conf.py
+++ conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -27,7 +28,10 @@ author = 'DesignSpark'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = [
+    "sphinx.ext.autosectionlabel",
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -36,17 +40,51 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['venv', '_build', 'Thumbs.db', '.DS_Store']
 
+# This is where we place substitutions, such as for Unicode characters.
+rst_epilog = """
+.. include:: /substitutions.txt
+"""
+
+# Allow same section headings and thus labels to be used across documents.
+autosectionlabel_prefix_document = True
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4
+}
+
+html_logo = 'images/DS_logo_200w.png'
+html_favicon = 'images/DS_favicon_32x32.png'
+
+html_show_sphinx = False
+
+root_doc = 'index'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = ['ds-custom.css']
+
+html_last_updated_fmt = '%b %d, %Y'
+
+html_context = {
+    'display_github': True,
+    'github_user': 'DesignSparkRS',
+    'github_repo': 'DSDocs',
+    'github_version': 'master/' 
+}
--- index.rst
+++ index.rst
@@ -1,20 +1,7 @@
-.. DesignSpark Documentation documentation master file, created by
-   sphinx-quickstart on Tue Oct 19 19:40:44 2021.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to DesignSpark Documentation's documentation!
-=====================================================
+Welcome to DesignSpark Documentation
+====================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
